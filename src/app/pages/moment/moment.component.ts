af3a2dc5a1aac489bc324801eaf0603375c0e6b2
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-03-05 10:33:21 +0000

    moment-page

---
+++ src/app/pages/moment/moment.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { MomentService } from 'src/app/services/moment.service';
+
+import { Moment } from 'src/app/Moment';
+
+import { environment } from 'src/environments/environment';
+
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-moment',
+  templateUrl: './moment.component.html',
+  styleUrls: ['./moment.component.css']
+})
+export class MomentComponent implements OnInit {
+  moment?: Moment;
+  baseApiUrl = environment.baseApiUrl
+
+  faTimes = faTimes;
+  faEdit = faEdit;
+
+  constructor(
+    private momentService: MomentService, 
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    const id = Number(this.route.snapshot.paramMap.get('id')); // pegando o id da rota.
+
+    this.momentService
+      .getMoment(id)
+      .subscribe((item) => (this.moment = item.data));
+  }
+
+}
