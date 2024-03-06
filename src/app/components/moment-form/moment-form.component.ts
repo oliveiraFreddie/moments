c2e31ba608782a86a97dd59567e8475fef23e484
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-03-06 12:33:35 +0000

    coment-form

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,30 +1,42 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+  styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent implements OnInit {
-  @Output() onSubmit = new EventEmitter<Moment>()
-  @Input() btnText!: string;
+  @Output() onSubmit = new EventEmitter<Moment>();
   @Input() momentData: Moment | null = null;
+  @Input() btnText!: string;
 
-  momentForm!: FormGroup
-  
+  image?: File;
 
-  constructor() { }
+  momentForm!: FormGroup;
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.momentForm = new FormGroup({
-      id: new FormControl(this.momentData ? this.momentData.id : ""),
-      title: new FormControl(this.momentData ? this.momentData.title : "", [Validators.required]),
-      description: new FormControl(this.momentData ? this.momentData.description : "", [Validators.required]),
-      image: new FormControl(''),
-    });
+    if (this.momentData) {
+      console.log(this.momentData);
+      this.momentForm = new FormGroup({
+        id: new FormControl(this.momentData.id),
+        title: new FormControl(this.momentData.title, [Validators.required]),
+        description: new FormControl(this.momentData.description, [
+          Validators.required,
+        ]),
+        image: new FormControl(''),
+      });
+    } else {
+      this.momentForm = new FormGroup({
+        id: new FormControl(''),
+        title: new FormControl('', [Validators.required]),
+        description: new FormControl('', [Validators.required]),
+        image: new FormControl(''),
+      });
+    }
   }
 
   get title() {
@@ -36,18 +48,18 @@ export class MomentFormComponent implements OnInit {
   }
 
   onFileSelected(event: any) {
-    const file: File = event.target.files[0]
+    const file: File = event.target.files[0];
 
-    this.momentForm.patchValue({image: file});
+    this.momentForm.patchValue({ image: event.target.files[0] });
   }
 
   submit() {
-    if(this.momentForm.invalid) {
+    if (this.momentForm.invalid) {
       return;
     }
-    console.log(this.momentForm.value)
+
+    console.log(this.momentForm.value);
 
     this.onSubmit.emit(this.momentForm.value);
   }
-
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,48 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { MomentService } from 'src/app/services/moment.service';
+
 import { Moment } from 'src/app/Moment';
+
 import { environment } from 'src/environments/environment';
 
-import {faSearch} from "@fortawesome/free-solid-svg-icons"
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  allMoments: Moment[] = []
-  moments: Moment[] = []
-  baseApiUrl = environment.baseApiUrl
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
-  faSearch = faSearch
   searchTerm: string = '';
+  faSearch = faSearch;
 
-  constructor(private momentService: MomentService ) { }
+  constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
     this.momentService.getMoments().subscribe((items) => {
-
-      const data = items.data
+      const data = items.data;
 
       data.map((item) => {
-        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
-      })
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
 
-
-      this.allMoments = data;
-      this.moments = data;
-    })
+      this.allMoments = items.data;
+      this.moments = items.data;
+    });
   }
 
   search(e: Event): void {
     const target = e.target as HTMLInputElement;
     const value = target.value;
 
-    this.moments = this.allMoments.filter((moment) => {
-      return moment.title.toLowerCase().includes(value)
-    });
+    this.moments = this.allMoments.filter((moment) =>
+      moment.title.toLowerCase().includes(value)
+    );
   }
-
 }
